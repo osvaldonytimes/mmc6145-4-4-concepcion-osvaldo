f75161d2e105c139d5ffab2a426a13a943ab31f3
Osvaldo Concepcion <osvaldoconcep@gmail.com>
2023-10-13 13:21:26 -0400

    Ability to add and remove books

--- src/pages/book/index.jsx
+++ src/pages/book/index.jsx
@@ -1,43 +1,42 @@
-import { Link, useParams, useNavigate, Navigate } from "react-router-dom"
-import { useBookContext } from "../../context/book"
-import styles from "./style.module.css"
-// TODO: import actions
+import { Link, useParams, useNavigate, Navigate } from "react-router-dom";
+import { useBookContext } from "../../context/book";
+import styles from "./style.module.css";
+import { actions } from "../../context/book/actions.js";
 
 export default function Book() {
-  const { bookId } = useParams()
-  const navigate = useNavigate()
-  // TODO: Use dispatch appropriately to add/remove books
-  const [{bookSearchResults, favoriteBooks}, dispatch] = useBookContext()
+  const { bookId } = useParams();
+  const navigate = useNavigate();
+  const [{ bookSearchResults, favoriteBooks }, dispatch] = useBookContext();
 
-  let isFavoriteBook = false
-  let book = favoriteBooks.find(book => book.id === bookId)
-  if (book)
-    isFavoriteBook = true
-  else
-    book = bookSearchResults.find(book => book.id === bookId)
-  if (!book) return <Navigate to='/favorites' />
+  let isFavoriteBook = false;
+  let book = favoriteBooks.find((book) => book.id === bookId);
+  if (book) isFavoriteBook = true;
+  else book = bookSearchResults.find((book) => book.id === bookId);
+  if (!book) return <Navigate to="/favorites" />;
+
+  const handleAddToFavorites = () => {
+    dispatch({ type: actions.ADD_BOOK, payload: { book } });
+  };
+
+  const handleRemoveFromFavorites = () => {
+    dispatch({ type: actions.REMOVE_BOOK, payload: { book } });
+  };
 
   return (
     <main>
-      <BookInfo isFavorite={isFavoriteBook} {...book}/>
+      <BookInfo isFavorite={isFavoriteBook} {...book} />
       <div className={styles.controls}>
-        {
-          isFavoriteBook
-          // TODO: add onClick function that calls dispatch to remove a book
-          ? <button>
-              Remove from Favorites
-            </button>
-          // TODO: add onClick function that calls dispatch to add a book
-          : <button>
-              Add to Favorites
-            </button>
-        }
-        <Link onClick={() => navigate(-1)}>
-          Return
-        </Link>
+        {isFavoriteBook ? (
+          <button onClick={handleRemoveFromFavorites}>
+            Remove from Favorites
+          </button>
+        ) : (
+          <button onClick={handleAddToFavorites}>Add to Favorites</button>
+        )}
+        <Link onClick={() => navigate(-1)}>Return</Link>
       </div>
     </main>
-  )
+  );
 }
 
 function BookInfo({
@@ -48,47 +47,70 @@ function BookInfo({
   isFavorite,
   pageCount,
   categories,
-  previewLink
+  previewLink,
 }) {
   return (
     <>
       <div className={styles.titleGroup}>
         <div>
-          <h1>{title}{isFavorite && <sup>⭐</sup>}</h1>
-          {
-            authors &&
-            <h2>By: {authors.join(", ").replace(/, ([^,]*)$/, ', and $1')}</h2>
-          }
-          {
-            categories &&
-            <h3>Category: {categories.join(", ").replace(/, ([^,]*)$/, ', and $1')}</h3>
-          }
+          <h1>
+            {title}
+            {isFavorite && <sup>⭐</sup>}
+          </h1>
+          {authors && (
+            <h2>By: {authors.join(", ").replace(/, ([^,]*)$/, ", and $1")}</h2>
+          )}
+          {categories && (
+            <h3>
+              Category:{" "}
+              {categories.join(", ").replace(/, ([^,]*)$/, ", and $1")}
+            </h3>
+          )}
         </div>
-        <a target="_BLANK"
+        <a
+          target="_BLANK"
           href={previewLink}
           className={styles.imgContainer}
-          rel="noreferrer">
-          <img src={thumbnail
-            ? thumbnail
-            : "https://via.placeholder.com/128x190?text=NO COVER"} alt={title} />
+          rel="noreferrer"
+        >
+          <img
+            src={
+              thumbnail
+                ? thumbnail
+                : "https://via.placeholder.com/128x190?text=NO COVER"
+            }
+            alt={title}
+          />
           <span>Look Inside!</span>
         </a>
       </div>
-      <p>Description:<br/>{description}</p>
+      <p>
+        Description:
+        <br />
+        {description}
+      </p>
       <p>Pages: {pageCount}</p>
       <div className={styles.links}>
         <span>Order online:</span>
-        <a target="_BLANK"
-          href={`https://www.amazon.com/s?k=${title} ${authors ? authors[0] : ""}`}
-          rel="noreferrer">
+        <a
+          target="_BLANK"
+          href={`https://www.amazon.com/s?k=${title} ${
+            authors ? authors[0] : ""
+          }`}
+          rel="noreferrer"
+        >
           Amazon
         </a>
-        <a target="_BLANK"
-          href={`https://www.barnesandnoble.com/s/${title} ${authors ? authors[0] : ""}`}
-          rel="noreferrer">
+        <a
+          target="_BLANK"
+          href={`https://www.barnesandnoble.com/s/${title} ${
+            authors ? authors[0] : ""
+          }`}
+          rel="noreferrer"
+        >
           Barnes & Noble
         </a>
       </div>
     </>
-  )
+  );
 }
